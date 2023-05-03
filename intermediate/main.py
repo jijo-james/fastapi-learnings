da41cb317a01bea97f6a32eac30c3504ac374a46
Jijo James <jj.jamesjijo@gmail.com>
2023-05-03 23:29:15 +0530

    intermediate/ http requests

--- intermediate/main.py
+++ intermediate/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -7,7 +7,13 @@ app = FastAPI()
 async def root():
     return {"msg": "Welcome to my APIs"}
 
+
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your latest post"}
 
+
+@app.post("/createposts")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"message": "Successfully created a post"}
